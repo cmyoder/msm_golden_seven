fa023ccbe572532baeb2d94fc2aa27ab2ee857c2
cmyoder <cmyoder@uchicago.edu>
2017-02-07 02:14:10 -0600

    did movies
    again, copy and paste

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,63 @@
 class MoviesController < ApplicationController
 
   def index
+    @list_of_movies = Movie.all
+  end
+
+  def show
+    @movie_id = params[:id].to_i
+    movie = Movie.find(@movie_id)
+    @movie_title = movie.title
+    @movie_description = movie.description
+    @movie_year = movie.year
+    @movie_duration = movie.duration
+    @movie_image_url = movie.image_url
+  end
+
+  def destroy
+    @id_to_destroy = params[:id]
+    m = Movie.find(@id_to_destroy)
+    m.destroy
+
+    redirect_to("/movies")
+  end
+
+  def new_form
 
   end
 
+  def create_row
+    m = Movie.new
+    m.title = params[:title]
+    m.description = params[:description]
+    m.year = params[:year]
+    m.duration = params[:duration]
+    m.image_url = params[:image_url]
+    m.save
 
+    redirect_to("/movies")
+  end
+
+  def edit_form
+    @id_to_edit = params[:id]
+    m = Movie.find(@id_to_edit)
+    @old_title = m.title
+    @old_image_url = m.image_url
+    @old_year = m.year
+    @old_duration = m.duration
+    @old_description = m.description
+  end
+
+  def update_row
+    @edit_id = params[:id]
+    m = Movie.find(@edit_id)
+    m.title = params[:title]
+    m.year = params[:year]
+    m.duration = params[:duration]
+    m.description = params[:description]
+    m.image_url = params[:image_url]
+    m.save
+
+    redirect_to("/movies/#{@edit_id}")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,13 +11,19 @@ Rails.application.routes.draw do
   get("/update_director/:id",    { :controller => "directors", :action => "update_row"})
 
   get("/actors",                 { :controller => "actors", :action => "index"})
-  get("/actors/new",          { :controller => "actors", :action => "new_form"})
-  get("/actors/:id",          { :controller => "actors", :action => "show"})
-  get("/actors/:id/edit",     { :controller => "actors", :action => "edit_form"})
-  get("/delete_actor/:id",    { :controller => "actors", :action => "destroy"})
-  get("/create_actor",        { :controller => "actors", :action => "create_row"})
-  get("/update_actor/:id",    { :controller => "actors", :action => "update_row"})
+  get("/actors/new",             { :controller => "actors", :action => "new_form"})
+  get("/actors/:id",             { :controller => "actors", :action => "show"})
+  get("/actors/:id/edit",        { :controller => "actors", :action => "edit_form"})
+  get("/delete_actor/:id",       { :controller => "actors", :action => "destroy"})
+  get("/create_actor",           { :controller => "actors", :action => "create_row"})
+  get("/update_actor/:id",       { :controller => "actors", :action => "update_row"})
 
   get("/movies",                 { :controller => "movies", :action => "index"})
+  get("/movies/new",             { :controller => "movies", :action => "new_form"})
+  get("/movies/:id",             { :controller => "movies", :action => "show"})
+  get("/movies/:id/edit",        { :controller => "movies", :action => "edit_form"})
+  get("/delete_movie/:id",       { :controller => "movies", :action => "destroy"})
+  get("/create_movie",           { :controller => "movies", :action => "create_row"})
+  get("/update_movie/:id",       { :controller => "movies", :action => "update_row"})
 
 end
